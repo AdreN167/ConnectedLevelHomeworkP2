f5362c77d453b9476bcaee7ef8e881746e48dcda
AdreN167 <zz11926557@gmail.com>
2021-02-24 21:50:05 +0600

    Домашнее задание по подключенному уровню (2 часть)

---
+++ ConnectedLevelHomeworkP2/Data/DataAccess.cs
@@ -0,0 +1,43 @@
+﻿using System.Data.SqlClient;
+using System;
+
+namespace ConnectedLevelHomeworkP2.Data
+{
+    public class DataAccess : IDisposable
+    {
+        private readonly SqlConnection connection;
+
+        public DataAccess()
+        {
+            connection = new SqlConnection();
+            connection.ConnectionString = "Server=WW\\MSSQLSERVER2017; Database=ConnectedLevelLesson; Trusted_Connection=True;";
+            connection.Open();
+        }
+
+        public void CreateTable()
+        {
+            var createTableScript = "create table gruppa (Id int not null identity, [Name] nvarchar(max) not null);";
+
+            using (var transaction = connection.BeginTransaction())
+            using (var command = new SqlCommand(createTableScript, connection))
+            {
+                
+                try
+                {
+                    command.Transaction = transaction;
+                    command.ExecuteNonQuery();
+                    transaction.Commit();
+                }
+                catch
+                {
+                    transaction.Rollback();
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            connection.Close();
+        }
+    }
+}
---
+++ ConnectedLevelHomeworkP2/UI/Program.cs
@@ -0,0 +1,14 @@
+﻿using ConnectedLevelHomeworkP2.Data;
+
+namespace ConnectedLevelHomeworkP2.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dataAccess = new DataAccess();
+
+            dataAccess.CreateTable();
+        }
+    }
+}
